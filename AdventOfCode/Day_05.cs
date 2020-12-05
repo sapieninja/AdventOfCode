c09e17643ddb8b7668aca76237332e94dd131758
Max Bowman <maxjbowman@gmail.com>
2020-12-05 11:33:26 +0000

    Made my code significantly cleaner (credit to @ThatOneArchGuy for telling me I could just find the missing ID)

--- AdventOfCode/Day_05.cs
+++ AdventOfCode/Day_05.cs
@@ -4,6 +4,7 @@ using AoCHelper;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace AdventOfCode
 {
@@ -19,61 +20,30 @@ namespace AdventOfCode
         public static string solve(bool solution2 = false)
         {    
             var scores = new List<int>();
-            var seats = new List<(int row, int column)>();
-            var row = 0;
             int score = 0;
-            string start = "";
-            string end = "";
-            var column = 0;
+            string seatcode = "";
             foreach (var pass in _input)
             {
-                start = pass.Substring(0,7);
-                start = start.Replace("B","1");
-                start = start.Replace("F","0");
-                row = Convert.ToInt32(start,2);
-                end = pass.Substring(7,3);
-                end = end.Replace("R","1");
-                end = end.Replace("L","0");
-                column = Convert.ToInt32(end, 2);
-                score = row * 8 + column;
+                seatcode = pass.Replace("B","1");
+                seatcode = seatcode.Replace("F","0");
+                seatcode = seatcode.Replace("R", "1");
+                seatcode = seatcode.Replace("L", "0");
+                score = Convert.ToInt32(seatcode, 2);
                 scores.Add(score);
                 //Now we look for a missing seat in every row
-                seats.Add((row,column));
             }
-
-            var doneRows = new List<int>();
-            var columns = new List<int>();
-            var sum = 0;
-
             if (solution2 == true)
             {
-                foreach (var seat in seats)
+                var missing = Enumerable.Range(1, 1024).Except(scores);
+                int i = 1;
+                foreach (var value in missing)
                 {
-                    row = seat.row;
-                    foreach (var subseat in seats)
+                    if (i != value)
                     {
-                        if (subseat.row == row)
-                        {
-                            columns.Add(subseat.column);
-                        }
+                        return value.ToString();
                     }
 
-                    if (columns.Count != 8)
-                    {    
-                        foreach (var columnvalue in columns)
-                        {
-                            sum += columnvalue;
-                        }
-
-                        if (sum != 28)
-                        {
-                            column = 28 - sum;
-                            return (row * 8 + column).ToString();
-                        }
-
-                        sum = 0;
-                    }
-                    columns = new List<int>();
+                    i++;
                 }
             }
 
