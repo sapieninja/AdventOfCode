1686c0aa6f47a135a2c2b02e0fce807ccbbec0f9
Max Bowman <maxjbowman@gmail.com>
2020-12-09 05:25:11 +0000

    Completed Day 9 of AoC in Python I was pretty pleased with my time for part 1, although I would have been faster had I been more familiar with list splicing as my spliced list was not the right length       --------Part 1--------   --------Part 2-------- Day       Time   Rank  Score       Time   Rank  Score   9   00:10:49   2449      0   00:18:16   1946      0

---
+++ AdventOfCode/Python/09.py
@@ -0,0 +1,33 @@
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+import queue
+def main():
+    ints = aoc_utils.readints()
+    start = 25
+    for x in range(start,len(ints)-1):
+        value = ints[x]
+        previous = ints[x-25:x]
+        if not sumto(value,previous):
+            print(findsumofrange(value,ints))
+            return value
+def findsumofrange(value,ints):
+    for x in range(len(ints)-1):
+        sum = ints[x]
+        counter = 1
+        while(sum < value):
+            sum += ints[x+counter]
+            counter += 1
+        if sum == value:
+            contiguous = ints[x:x+counter]
+            return max(contiguous) + min(contiguous)
+def sumto(value,list):
+    for x in list:
+        for y in list:
+            if x + y == value:
+                return True
+    return False
+print(main())
