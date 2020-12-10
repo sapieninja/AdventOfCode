7bcf4088bc28b8c0cb78a81af1ba875bd28ed7e7
Max Bowman <maxjbowman@gmail.com>
2020-12-10 07:09:13 +0000

    Cleaned code up a little bit

--- AdventOfCode/Python/10.py
+++ AdventOfCode/Python/10.py
@@ -22,19 +22,13 @@ def noways(inputs,start_digit,maximum):
     if start_digit in cache:
         return cache[start_digit]
     no = 0
-    if start_digit == -1:
-        start = 0
-    else:
-        start = inputs[start_digit]
+    if start_digit == -1:start = 0
+    else:start = inputs[start_digit]
     if start_digit == len(inputs) - 1: return 1
-    if maximum - inputs[start_digit] <= 3 and start_digit != -1:
-        no += 1
-    if inputs.count(start+1) == 1:
-        no += noways(inputs,inputs.index(start+1),maximum)
-    if inputs.count(start+2) == 1:
-        no += noways(inputs,inputs.index(start+2),maximum)
-    if inputs.count(start+3) ==1:
-        no += noways(inputs,inputs.index(start+3),maximum)
+    if maximum - inputs[start_digit] <= 3 and start_digit != -1:no += 1
+    for x in range(1,4):
+        if inputs.count(start+x) == 1:
+            no += noways(inputs,inputs.index(start+x),maximum)
     cache[start_digit] = no
     return no
 print(main())
