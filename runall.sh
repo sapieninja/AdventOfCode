cc4c9dd0e65b2f429cc0b9b3443a115fdf1f215d
Max Bowman <maxjbowman@gmail.com>
2020-12-11 14:50:23 +0000

    Changed Formatting

--- runall.sh
+++ runall.sh
@@ -3,6 +3,6 @@ python3 --version
 for x in $(seq -f "%02g" 1 "$today") 
 do
 echo "Day:$x"
-echo "___________________________"
 time -f %E python3 ./AdventOfCode/Python/$x.py
+echo "___________________________"
 done
