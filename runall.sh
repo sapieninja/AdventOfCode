afbdc5856215f7ec394458be161bcdab8c8c7e18
Max Bowman <maxjbowman@gmail.com>
2020-12-11 14:49:00 +0000

    Added time counting to CI

--- runall.sh
+++ runall.sh
@@ -3,5 +3,6 @@ python3 --version
 for x in $(seq -f "%02g" 1 "$today") 
 do
 echo "Day:$x"
-python3 ./AdventOfCode/Python/$x.py
+echo "___________________________"
+time -f %E python3 ./AdventOfCode/Python/$x.py
 done
