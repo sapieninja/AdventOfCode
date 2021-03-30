3e63aa9067078b19c90cdc7cdb9285232f45c6b2
Max Bowman <maxjbowman@gmail.com>
2021-03-30 13:20:37 +0100

    Reorganised repo

--- runall.sh
+++
@@ -1,8 +0,0 @@
-today=$(date +'%-d')
-python3 --version
-for x in $(seq -f "%02g" 1 "$today") 
-do
-echo "Day:$x"
-time -f %E python3 ./AdventOfCode/Python/$x.py
-echo "___________________________"
-done
