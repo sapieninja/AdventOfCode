3c1ad168a631d96f5866347e982952c014bbb1fd
Max Bowman <maxjbowman@gmail.com>
2021-12-06 06:26:32 +0000

    Slightly worse performance today but still going strong       -------Part 1--------   -------Part 2-------- Day       Time  Rank  Score       Time  Rank  Score   6   00:05:42   914      0   00:31:03  3697      0
    Signed-off-by: Max Bowman <maxjbowman@gmail.com>

--- Python/Python21/06.py
+++ Python/Python21/06.py
@@ -8,4 +8,16 @@ from collections import *
 from copy import deepcopy
 import random
 import re
-lines = aoc_utils.readlines()
+nums = aoc_utils.readSplittedIntLine()
+density = defaultdict(int)
+for x in nums:
+    density[x] += 1
+print(density)
+for x in range(256):
+    for key in range(-1,max(density)):
+        density[key] = density[key+1]
+    density[max(density)] = 0
+    density[6] = density[6] +  density[-1]
+    density[8] = density[8] +  density[-1]
+    density[-1] = 0
+print(sum(density.values()))
