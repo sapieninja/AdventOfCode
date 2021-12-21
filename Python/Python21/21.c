da02174e0da4b2210812bc0e66a4a6b99f51f084
Max Bowman <maxjbowman@gmail.com>
2021-12-21 10:24:38 +0000

    Made the c solution better
    Signed-off-by: Max Bowman <maxjbowman@gmail.com>

--- Python/Python21/21.c
+++ Python/Python21/21.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-long cachea[20000000];
-long cacheb[20000000];
-int amalgamate(int posa, int posb, int scorea, int scoreb, int turn)
+long cachea[10000000];
+long cacheb[10000000];
+int amalgamate(int posa, int posb, int scorea, int scoreb)
 {
     int out;
     out = 0;
@@ -9,15 +9,14 @@ int amalgamate(int posa, int posb, int scorea, int scoreb, int turn)
     out += posb * 10;
     out += scorea * 1000;
     out += scoreb * 100000;
-    out += (turn % 2) * 10000000;
     return out;
 }
 
-void noways(int posa, int posb, int scorea, int scoreb, int turn, long *aways, long *bways, int rolls[])
+void noways(int posa, int posb, int scorea, int scoreb, long *aways, long *bways, int rolls[])
 //aways and bways are pointers so two values can be returned
 {
     int amalgamation;
-    amalgamation = amalgamate(posa, posb, scorea, scoreb, turn);
+    amalgamation = amalgamate(posa, posb, scorea, scoreb);
     if (cachea[amalgamation] != 0)
     {
         *aways = cachea[amalgamation];
@@ -49,39 +48,29 @@ void noways(int posa, int posb, int scorea, int scoreb, int turn, long *aways, l
         cacheb[amalgamation] = 1;
         return;
     }
-    if (turn % 2 == 0)
+    for (int i = 0; i < 27; i++)
     {
-        for (int i = 0; i < 27; i++)
-        {
-            noways((posa + rolls[i] - 1) % 10 + 1, posb, scorea + (posa + rolls[i] - 1) % 10 + 1, scoreb, turn + 1, &aget, &bget, rolls);
-            totala += aget;
-            totalb += bget;
-        }
-        *aways = totala;
-        *bways = totalb;
-        cachea[amalgamation] = totala;
-        cacheb[amalgamation] = totalb;
-        return;
+        noways(posb, (posa + rolls[i] - 1) % 10 + 1,scoreb, scorea + (posa + rolls[i] - 1) % 10 + 1, &bget, &aget, rolls);
+        totala += aget;
+        totalb += bget;
     }
-    else
-    {
-        for (int i = 0; i < 27; i++)
-        {
-            noways(posa, (posb + rolls[i] - 1) % 10 + 1, scorea, scoreb + (posb + rolls[i] - 1) % 10 + 1, turn + 1, &aget, &bget, rolls);
-            totala += aget;
-            totalb += bget;
-        }
-        *aways = totala;
-        *bways = totalb;
-        cachea[amalgamation] = totala;
-        cacheb[amalgamation] = totalb;
-        return;
-    }
+    *aways = totala;
+    *bways = totalb;
+    cachea[amalgamation] = totala;
+    cacheb[amalgamation] = totalb;
+    return;
 }
 int main()
 {
     int rolls[] = {3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9};
     long aways, bways;
-    noways(10, 1, 0, 0, 0, &aways, &bways, rolls);
-    printf("%ld,%ld\n", aways, bways);
+    noways(10, 1, 0, 0, &aways, &bways, rolls);
+    if (aways>bways)
+    {
+        printf("%ld\n",aways);
+    }
+    if (bways>aways)
+    {
+        printf("%ld\n",bways);
+    }
 }
