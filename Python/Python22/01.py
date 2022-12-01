715bb14bb1d39e91d2a40f67856d8beec4a22f9d
max <maxjbowman@gmail.com>
2022-12-01 11:48:05 +0000

    Made 01.py nice and neat

--- Python/Python22/01.py
+++ Python/Python22/01.py
@@ -1,18 +1,4 @@
 import aoc_utils
-import itertools
-import functools
-import operator
-import networkx as nx
-import math
-from collections import *
-from copy import deepcopy
-import random
-import re
-from string import ascii_lowercase as alph
-nums = aoc_utils.readparagraphs()
-values = []
-for paragraph in nums:
-    total = sum(map(int,paragraph))
-    values.append(total)
-values.sort()
+values = sorted(list(map(sum,aoc_utils.readIntParagraphs())))
+print(values[-1])
 print(values[-1]+values[-2]+values[-3])
